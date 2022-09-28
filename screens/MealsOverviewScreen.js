4d3387d451a53a34c44109c38e9dfb127cc16935
JoacoMaurtua <joacomaurtua@gmail.com>
2022-09-27 23:54:11 -0500

    displayMEALS function

--- screens/MealsOverviewScreen.js
+++ screens/MealsOverviewScreen.js
@@ -1,13 +1,29 @@
-import {View, Text, StyleSheet} from 'react-native';
+import {View, Text, StyleSheet, FlatList} from 'react-native';
+
+import {MEALS} from '../data/dummy-data';
 
 const MealsOverviewScreen = ({route}) => {
 
   const id = route.params.categoryID;
   const title = route.params.categoryName;
 
+  //Hacer Match entre las comidas y a que categorias pertenecen
+  const displayedMeals = MEALS.filter((mealItem) => (
+    mealItem.categoryIds.indexOf(id) >= 0
+  ));
+
+  function renderMealItem(itemData){
+
+  }
+
   return (
     <View style = {styles.conrainer}>
-      <Text>{title} - {id}</Text>
+      <FlatList
+        data={displayedMeals}
+        keyExtractor={(item) => item.id}
+        renderItem={renderMealItem}
+      
+      />
     </View>
   )
 }
@@ -22,3 +38,5 @@ const styles = StyleSheet.create({
 })
 
 /* Cualquier componenete registrado como screen puede acceder como prop a route */
+
+/* Para utilizar route en un componente no registrado como screen, se puede usar useRoute */
