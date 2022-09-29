d9857f4ea94d12bc6a67d428f3a3e0eba5ca0917
JoacoMaurtua <joacomaurtua@gmail.com>
2022-09-29 13:57:16 -0500

    primeros estilos de MealDetailsScreen

--- screens/MealDetailsScreen.js
+++ screens/MealDetailsScreen.js
@@ -2,10 +2,11 @@ import { useEffect } from 'react';
 import { View, Text, StyleSheet, Image } from 'react-native';
 import MealDetails from '../components/MealDetails';
 import { MEALS } from '../data/dummy-data';
+import Subtitle from '../components/Subtitle';
 
 const MealDetailsScreen = ({ route, navigation }) => {
   const id = route.params.id;
- 
+
   const selectedMeal = MEALS.find((meal) => meal.id === id);
 
   //Generar titulos dinamicamente
@@ -18,25 +19,28 @@ const MealDetailsScreen = ({ route, navigation }) => {
   return (
     <View>
       <Image source={{ uri: selectedMeal.imageUrl }} style={styles.image} />
-      <Text>{selectedMeal.title}</Text>
+      <Text style={styles.title}>{selectedMeal.title}</Text>
       <MealDetails
         duration={selectedMeal.duration}
         complexity={selectedMeal.complexity}
         affordability={selectedMeal.affordability}
       />
-      <Text>Ingredients</Text>
+
+      <Subtitle>Ingredients</Subtitle>
+
       <View>
         {selectedMeal.ingredients.map((ingredient) => (
           <Text key={ingredient}>{ingredient}</Text>
         ))}
       </View>
-      <Text>Steps</Text>
+
+      <Subtitle>Steps</Subtitle>
+
       <View>
         {selectedMeal.steps.map((step) => (
           <Text key={step}>{step}</Text>
         ))}
       </View>
-
     </View>
   );
 };
@@ -46,6 +50,15 @@ export default MealDetailsScreen;
 const styles = StyleSheet.create({
   image: {
     width: '100%',
-    height: 250,
+    height: 300,
   },
+
+  title: {
+    fontWeight: 'bold',
+    fontSize: 24,
+    margin: 8,
+    textAlign: 'center',
+  },
+
+  
 });
